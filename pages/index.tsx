4baf06ed2f656651e83cf1cb0e505bc57abe68c9
Ryan Irilli <ryanirilli@gmail.com>
2022-05-17 17:21:43 -0700

    add splashpage

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import SummerSetType from "../components/SummerSetType";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Summer Set '22</title>
+        <meta
+          name="description"
+          content="Summer Set 2022, an end of summer celebration"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid
+        placeContent="center"
+        bgGradient="linear(to-b, #6875B3, #7490C1, #008AD0)"
+        bgSize="cover"
+        minH="100vh"
+      >
+        <GridItem w={96} color="white">
+          <SummerSetType />
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
